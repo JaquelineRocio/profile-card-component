5b3b7dfa109dff1919f48f7f1223f09d35d5fc41
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-09-11 08:27:54 -0500

    style: fix border radius

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import "./App.css";
 import ProfileCard from "./components/ProfileCard/ProfileCard";
 
 function App() {
-  return <ProfileCard />;
+  return (
+    <>
+      <div className="top-left-image"></div>
+      <div className="bottom-right-image"></div>
+      <ProfileCard />
+    </>
+  );
 }
 
 export default App;
